c8afdd0106e44892bd38a64d413f69e3ee5a727d
rohanjha02 <rohan.jha@bds.christuniversity.in>
2024-04-22 18:25:33 +0530

    main

---
+++ receipe-main/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class VegiConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'vegi'
---
+++ receipe-main/utils.py
@@ -0,0 +1,8 @@
+from home.models import Student
+import time
+
+def run_this_function():
+    print("Run This Function")
+    time.sleep(2)
+    print("Exit Function")
+
